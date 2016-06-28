4b1e4bded1071090e34ecb15d84e324e13c7d358
Quino <quino@acidtango.com>
2016-06-28 12:59:00 +0200

    Adds redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['http://live-video-demo.herokuapp.com/', 'http://live-video-demo.herokuapp.com/']
+  config.web_socket_server_url = "wss://live-video-demo.herokuapp.com" 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
